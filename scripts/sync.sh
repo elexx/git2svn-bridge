96a238671ef9e83583186869669d9ba739ea3f7e
Alexander Falb <fal3xx@gmail.com>
2020-07-23 14:32:14 +0200

    fix "commandline too long" for huge commit message

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -15,8 +15,10 @@ for d in /data/repo_* ; do
 
 	git fetch origin master
 
-	MESSAGE=$(git log --pretty=format:'%h - (%ai) %s - %aN %n%w(64,16,16)%-b' HEAD..origin/master)
-	git merge --allow-unrelated-histories --no-ff --no-log -m "$MESSAGE" origin/master
+	MESSAGE_FILE=$(mktemp)
+	git log --pretty=format:'%h - (%ai) %s - %aN %n%w(64,16,16)%-b' HEAD..origin/master > "$MESSAGE_FILE"
+	git merge --allow-unrelated-histories --no-ff --no-log --file "$MESSAGE_FILE" origin/master
+	rm "$MESSAGE_FILE"
 
 	git svn dcommit
 done
